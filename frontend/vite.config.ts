797667ee19e0770c6a0a1943efee48ed42e13a69
hsrmoa <guswnd031@hanmail.net>
2026-06-22 14:15:49 +0900

    20260622/한소라/포트번호 3000번으로 고정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,10 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   build: {
     outDir: '../src/main/resources/static',
     emptyOutDir:true
