0cc7b485032b036934e09113a2d0e64821c92ce0
hsrmoa <guswnd031@hanmail.net>
2026-06-25 16:26:44 +0900

    Swagger 권한 테스트

--- src/main/java/com/cshbk/cshmint/config/SwaggerConfig.java
+++ src/main/java/com/cshbk/cshmint/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.cshbk.cshmint.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,12 +18,21 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
   @Bean
+
   public OpenAPI openAPI() {
+
+    String securitySchemeName = "bearerAuth";
+
     return new OpenAPI()
-      .info(new Info()
-              .title("CSH MINT API")
-              .description("CSH MINT API 명세서")
-              .version("v1.0.0")
-      );
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+          .components(new Components()
+                   .addSecuritySchemes(securitySchemeName,
+                            new SecurityScheme()
+                                    .name(securitySchemeName)
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("bearer")
+                                    .bearerFormat("JWT")
+                    )
+            );
   }
 }
