4bffad9c1cc65044394480a989c42abc309a5d4f
hsrmoa <guswnd031@hanmail.net>
2026-07-17 16:20:51 +0900

    20260717/한소라/아이콘 정리

--- frontend/src/components/common/icon/iconCommon.ts
+++ frontend/src/components/common/icon/iconCommon.ts
@@ -1,18 +1,13 @@
-import SearchIcon from "@/components/common/icon/SearchIcon.tsx";
-import CalendarIcon from "@/components/common/icon/CalendarIcon.tsx";
-import Crown from "@/components/common/icon/Crown.tsx";
-import MinusIcon from "@/components/common/icon/MinusIcon.tsx";
-import PlusIcon from "@/components/common/icon/PlusIcon.tsx";
-import SettingIcon from "@/components/common/icon/SettingIcon.tsx"
+import {LuSearch, LuCalendarDays, LuChevronDown, LuCircleMinus, LuCirclePlus, LuSettings2} from "react-icons/lu";
 
 /**
  * 공통 ICONS를 지정한 정보
  */
 export const Icons = {
-  search: SearchIcon,       // 검색버튼
-  calendar: CalendarIcon,   // 달력아이콘
-  crown: Crown,             // 화살표
-  minus: MinusIcon,         // 마이너스버튼
-  plus: PlusIcon,           // 플러스버튼
-  settings: SettingIcon   // 설정버튼
+  search: LuSearch,       // 검색버튼
+  calendar: LuCalendarDays,   // 달력아이콘
+  crown: LuChevronDown,             // 화살표
+  minus: LuCircleMinus,         // 마이너스버튼
+  plus: LuCirclePlus,           // 플러스버튼
+  settings: LuSettings2   // 설정버튼
 };
